155c022e3abde1e85e46ce03dc2158f242fe7d7c
Michael Stott <michael@thestottfamily.uk>
2024-06-18 17:35:12 +0100

    Added Menu Animations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,11 +2,11 @@ var active = false;
 
 function menuOpen() {
     if (active === false) {
-        document.getElementById('mob-nav').style.display = 'block';
+        document.getElementById('mob-nav').classList = 'mob-nav show';
         document.getElementById('hamburger').src = "./images/close_24dp_FILL0_wght400_GRAD0_opsz24.svg"
         active = true;
     } else {
-        document.getElementById('mob-nav').style.display = 'none';
+        document.getElementById('mob-nav').classList = 'mob-nav hide';
         document.getElementById('hamburger').src = "./images/menu_24dp_FILL0_wght400_GRAD0_opsz24.svg"
         active = false;
     }
