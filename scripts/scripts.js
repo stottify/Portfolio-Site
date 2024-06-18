8ab7def87a956ad2d9551bd9ccf02f25fafd384c
Michael Stott <michael@thestottfamily.uk>
2024-06-18 16:36:06 +0100

    Mobile menu added with basic js function

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+var active = false;
+
+function menuOpen() {
+    if (active === false) {
+        document.getElementById('mob-nav').style.display = 'block';
+        document.getElementById('hamburger').src = "./images/close_24dp_FILL0_wght400_GRAD0_opsz24.svg"
+        active = true;
+    } else {
+        document.getElementById('mob-nav').style.display = 'none';
+        document.getElementById('hamburger').src = "./images/menu_24dp_FILL0_wght400_GRAD0_opsz24.svg"
+        active = false;
+    }
+}
