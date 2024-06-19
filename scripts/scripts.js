80322a45ffec4560bc8afb77ec0eb1f43cb04414
Michael Stott <michael@thestottfamily.uk>
2024-06-19 15:26:07 +0100

    Added colour changing button functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,3 +11,18 @@ function menuOpen() {
         active = false;
     }
 }
+
+function getRandomColor() {
+    const randomColors =["#A64E5E","#9BA64B","#569FA5","#CC7714","#D93D04"];
+    let chosenColor = randomColors[Math.floor(Math.random() * 5)];
+    return chosenColor;
+}
+
+function setRandomColor() {
+    let newColor = getRandomColor();
+    document.getElementById("a-what-button").style.backgroundColor = newColor;
+}
+
+function resetColor() {
+    document.getElementById("a-what-button").style.backgroundColor = "#1e1e1e";
+};
