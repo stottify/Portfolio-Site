c46eadee197d9c99cdc03a2a06d0edb3ad248594
Michael Stott <michael@thestottfamily.uk>
2024-06-20 11:32:40 +0100

    Added jQuery to allow global button color change

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,19 +10,16 @@ function menuOpen() {
         document.getElementById('hamburger').src = "./images/menu_24dp_FILL0_wght400_GRAD0_opsz24.svg"
         active = false;
     }
-}
-
-function getRandomColor() {
-    const randomColors =["#A64E5E","#9BA64B","#569FA5","#CC7714","#D93D04"];
-    let chosenColor = randomColors[Math.floor(Math.random() * 5)];
-    return chosenColor;
-}
-
-function setRandomColor() {
-    let newColor = getRandomColor();
-    document.getElementById("a-what-button").style.backgroundColor = newColor;
-}
-
-function resetColor() {
-    document.getElementById("a-what-button").style.backgroundColor = "#1e1e1e";
 };
+
+let colours = ["pink","green","blue","orange","dark-orange"];
+function getRandomColour() {
+    return colours[Math.floor(Math.random() * colours.length)];
+};
+$(document).ready(function() {
+    $(".button").on("mouseover" , function() {
+                $(this).addClass(getRandomColour());
+            }).on("mouseout", function() {
+                $(this).removeClass("pink green blue orange dark-orange");
+            })
+});
